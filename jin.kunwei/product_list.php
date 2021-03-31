5b3be4601838a35230299e7304f2800fc9b4c959
kunweidesign <78998344+kunweidesign@users.noreply.github.com>
2021-03-30 19:27:14 -0700

    Add files via upload

---
+++ jin.kunwei/product_list.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Product List</title>
+
+	<meta name="viewport" content="width=device-width">	
+	<link rel="stylesheet" href="lib/css/styleguide.css">
+	<link rel="stylesheet" href="lib/css/gridsystem.css">
+	<link rel="stylesheet" href="css/storetheme.css">
+
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+	<?php include "parts/navbar.php"; ?>
+
+	<div class="container">	
+		<div class="card soft">	
+			<h2>Product List</h2>
+			<ul>
+				<li><a href="product_item.php">product 1</a></li>
+				<li><a href="product_item.php">product 2</a></li>
+				<li><a href="product_item.php">product 3</a></li>
+				<li><a href="product_item.php">product 4</a></li>
+			</ul>	
+		</div>
+	</div>
+
+</body>
+</html>
+
+
+
+
