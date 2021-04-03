9e8b928b49f465335e58b65ed2d595d8e8baa8ef
Rachel Lum <rachellauren.lum@gmail.com>
2021-04-03 13:32:13 -0700

    php pages

---
+++ lum.rachel/index.php
@@ -0,0 +1,20 @@
+<! DOCTYPE html>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Product Item</title>
+
+	<?php include "parts/meta.php"; ?>
+
+</head>
+<body>
+
+	<?php include "parts/navbar.php"; ?>
+
+	<h2 class="container">Homepage</h2>
+
+
+</body>
+</html>
---
+++ lum.rachel/product_item.php
@@ -0,0 +1,33 @@
+<! DOCTYPE html>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Product Item</title>
+
+	<?php include "parts/meta.php"; ?>
+
+</head>
+<body>
+
+	<?php include "parts/navbar.php"; ?>
+
+	<div class="container">
+		<p><a href="product_list.php">Back</a></p>
+	</div>
+
+	<div class="container">
+		<div class="card soft">
+			<h2>Product Item</h2>
+
+				<p>This is item # <?= $_GET['id'] ?></p>
+			
+		</div>
+	</div>
+
+
+
+
+</body>
+</html>
