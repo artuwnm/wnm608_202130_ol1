f2b46cbf8ab12f3213935e150b62c86730afb2c9
vwang117 <78834354+vwang117@users.noreply.github.com>
2021-04-11 01:46:41 -0700

    Add files via upload

---
+++ parts/navbar.php
@@ -0,0 +1,19 @@
+<header class="navbar">
+		<div class="container display-flex">
+			<div class="flex-none">
+				<h1>Store</h1>
+			</div>
+			<div class="flex-stretch"></div>
+			<nav class="nav nav-flex flex-none">
+			<ul class="container display-flex">
+				<!-- li*3>a[href=#]>{Link $} -->
+				<li><a href="index.html">Home</a></li>
+				<li><a href="product_list.php">Store</a></li>
+				<li><a href="inspiration.php">Inspiration</a></li>
+				<li><a href="cart.php">Cart</a></li>
+			</ul>
+					
+					
+			</nav>
+		</div>
+	</header>
