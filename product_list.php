38ce5553d259094fe32ffab2d7ecbc358481b44b
vwang117 <78834354+vwang117@users.noreply.github.com>
2021-05-02 08:44:31 -0700

    Add files via upload

--- product_list.php
+++ product_list.php
@@ -1,3 +1,14 @@
+<?php 
+
+
+include_once "lib/php/functions.php";
+include_once "parts/templates.php";
+
+
+ ?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -29,9 +40,7 @@
 
 			<?php 
 
-			include_once "lib/php/functions.php";
-			include_once "parts/templates.php";
-
+			
 			$result = makeQuery(
 				makeConn(),
 				"
