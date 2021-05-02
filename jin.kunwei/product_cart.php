79ef58e8aec02c39c6d0a438460ab21209005acd
kunweidesign <78998344+kunweidesign@users.noreply.github.com>
2021-05-02 05:13:21 -0700

    Add files via upload

--- jin.kunwei/product_cart.php
+++ jin.kunwei/product_cart.php
@@ -3,8 +3,9 @@
 	include_once "lib/php/functions.php";
 	include_once "parts/templates.php";
 
-	$cart = makeQuery(makeConn(),"SELECT * FROM `products` WHERE `id` IN (3,11,5)");
+	//$cart = makeQuery(makeConn(),"SELECT * FROM `products` WHERE `id` IN (3,11,5)");
 
+	$cart_items = getCartItems();
 	
 ?>
 
@@ -13,7 +14,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Product Item</title>
+	<title>Cart Page</title>
 
 	<?php include "parts/meta.php"; ?>
 </head>
@@ -26,25 +27,11 @@
 			<div class="grid gap">
 				<div class="col-xs-12 col-md-7">
 					<div class="card soft">
-						<?= array_reduce($cart,'cartListTemplate') ?>	
+						<?= array_reduce($cart_items,'cartListTemplate') ?>	
 					</div>
 				</div>	
 				<div class="col-xs-12 col-md-5">
-					<div class="card soft flat">
-						<div class="card-section display-flex">
-							<div class="flex-stretch">Sub Total</div>
-							<div class="flex-none cart_subtotal">&dollar;1196.97</div>
-						</div>
-						<div class="card-section display-flex">
-							<div class="flex-stretch">Taxes</div>
-							<div class="flex-none cart_subtotal">&dollar;104.73</div>
-						</div>
-						<div class="card-section display-flex">
-							<div class="flex-stretch">Total</div>
-							<div class="flex-none cart_subtotal">&dollar;1301.70</div>
-						</div>												
-					</div>
-					<a href="product_checkout.php" class="form-button">Check Out</a>
+					<?= cartTotals() ?>
 				</div>
 			</div>			
 		</div>
--- jin.kunwei/product_list.php
+++ jin.kunwei/product_list.php
@@ -1,3 +1,9 @@
+<?
+
+		include_once "lib/php/functions.php";   
+		include_once "parts/templates.php";
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -22,8 +28,7 @@
 
 		<?php 
 
-		include_once "lib/php/functions.php";   
-		include_once "parts/templates.php";
+
 
 		$result = makeQuery(
 			makeConn(),
