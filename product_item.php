f7526dc099934c05600c897a365a571039e80cb8
vwang117 <78834354+vwang117@users.noreply.github.com>
2021-05-02 08:44:07 -0700

    Add files via upload

--- product_item.php
+++ product_item.php
@@ -1,18 +1,19 @@
 <?php 
 
-include_once "lib/php/functions.php";
+	include_once "lib/php/functions.php";
 
-$product = makeQuery(makeConn(),"SELECT * FROM `products` WHERE `id`=".$_GET['id'])[0];
+	$product = makeQuery(makeConn(), "SELECT * FROM `products` WHERE `id` = ".$_GET['id'])[0];
 
-$images = explode(",",$product->images);
+$images = explode(",", $product->images);
 
-$image_elements = array_reduce($images,function($r,$o){
-	return $r."<img src='img/$o'>";
-});
+	$image_elements = array_reduce($images, function($r,$o){
+		return $r."<img src='img/$o'>";
+
+	});
 
 
 
-//print_p($product);
+//print_p($_SESSION);
 
 
 
@@ -48,49 +49,84 @@ $image_elements = array_reduce($images,function($r,$o){
 					</div>
 				</div>
 			</div>
+
+
+
+
+
+
+
+
+
+
 			<div class="col-xs-12 col-md-5">
-				<div class="card soft flat">
+				<form class="card soft flat" method="post" action="cart_actions.php?action=add-to-cart">
+
+					<input type="hidden" name="product-id" value="<?= $product->id ?>">
+
+
 					<div class="card-section">
-						<h2 class="product-title"><?= $product->name ?></h2>
+						<h2 class="product-name"><?= $product->name ?></h2>
 						<h4 class="product-category">Category: <?= $product->category ?></h4>
 						<h2 class="product-price">&dollar;<?= $product->price ?></h2>
 					</div>
 
 					<div class="card-section">
-						<label for="product-amount" class="form-label">Amount</label>
-						<div class="form-select" id="product-amount">
-							<select>
-								<option>1</option>
-								<option>2</option>
-								<option>3</option>
-								<option>4</option>
-								<option>5</option>
-								<option>6</option>
-								<option>7</option>
-								<option>8</option>
-								<option>9</option>
-								<option>10</option>
-							</select>
+						<div class="form-control">
+							<label for="product-amount" class="form-label">Amount</label>
+							<div class="form-select">
+								<select id="product-amount" name="product-amount">
+									<option>1</option>
+									<option>2</option>
+									<option>3</option>
+									<option>4</option>
+									<option>5</option>
+									<option>6</option>
+									<option>7</option>
+									<option>8</option>
+									<option>9</option>
+									<option>10</option>
+								</select>
+							</div>
 						</div>
-					</div>
 
+						<div class="form-control">
+							<label for="product-color" class="form-label">Color</label>
+							<div class="form-select">
+								<select id="product-color" name="product-color">
+									<option>red</option>
+									<option>green</option>
+												
+								</select>
+							</div>
+							</div>
 
-
-
+						</div>
 					<div class="card-section center">
-						<a href="product_added_to_cart.php?id=<?= $product->id ?>" class="form-button">Add To Cart</a>
+						<input type="submit" class="form-button" value="Add To Cart">
 					</div>
 					
-				</div>
+				</form>
 			</div>
-
 		</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 		<div class="card soft dark">
 			<p><?= $product->description ?></p>
 		</div>
-
-
 
 	</div>
 
