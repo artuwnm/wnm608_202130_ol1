791d8fa12f04a14f4d4714eac5e16814219e7c41
changxdesign <78996484+changxdesign@users.noreply.github.com>
2021-05-19 04:09:50 -0700

    Add files via upload

---
+++ xu.chang/navbar.php
@@ -0,0 +1,28 @@
+<?php
+	
+include_once "lib/php/functions.php";
+
+?>
+	<input type="checkbox" id="menu" class="hidden">
+	<header class="navbar">
+		<div class="container display-flex">
+			<div class="flex-none">
+				<h1>IPHONING</h1>
+			</div>
+			<div class="flex-stretch"></div>
+			<div class="flex-none menu-button">
+				<label for="menu">&equiv;</label>
+			</div>			
+			<nav class="nav nav-flex flex-none">
+				<ul class="container display-flex">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="product_list.php">Store</a></li> 
+					<li><a href="product_cart.php"> 
+                    	
+                    	Cart<span><?= makeCartBadge(); ?></span> 
+                    </a></li>
+
+				</ul>
+			</nav>
+		</div>
+	</header>
