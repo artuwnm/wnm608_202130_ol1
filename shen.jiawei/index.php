f4ad7d362a56e1a1f7d82f391b62f07d9082f59c
hi94740 <hi94740@qq.com>
2021-05-24 01:40:52 +0800

    Assignment 14.1

--- shen.jiawei/index.php
+++ shen.jiawei/index.php
@@ -10,10 +10,28 @@
       <img class="hero" src="https://via.placeholder.com/1200x400?text=Ads" alt="ads">
     </div>
   </section>
-  <?php $section_title = "Featured" ?>
-  <?php include "parts/placeholder-item-section.php" ?>
-  <?php $section_title = "Discounts" ?>
-  <?php include "parts/placeholder-item-section.php" ?>
+  <section class="padded">
+    <h2>Featured</h2>
+    <div class="grid gap" style="height: 233px;">
+      <?php
+        include_once "lib/php/auth.php";
+        $db = db_joystore();
+        include_once "lib/php/functions.php";
+        include_once "lib/php/templates.php";
+        $featured_products = queryAll("SELECT * FROM `products` ORDER BY rand() LIMIT 3");
+        array_walk($featured_products,"item_card",4);
+      ?>
+    </div>
+  </section>
+  <section class="padded">
+    <h2>Discounts</h2>
+    <div class="grid gap" style="height: 233px;">
+      <?php
+        $discounted_products = queryAll("SELECT * FROM `products` WHERE `price` > `current_price` ORDER BY rand() LIMIT 3");
+        array_walk($discounted_products,"item_card",4);
+      ?>
+    </div>
+  </section>
   <section class="padded">
     <h2>About Us</h2>
     <div class="card soft flex center">
