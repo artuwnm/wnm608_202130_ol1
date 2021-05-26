0dfcffd90fdcee4509514aeaf940ff2acea78f80
akshatsonar <78786935+akshatsonar@users.noreply.github.com>
2021-05-26 11:50:06 +0530

    Update auth.php

--- sonar.akshat/php/auth.php
+++ sonar.akshat/php/auth.php
@@ -4,7 +4,7 @@ function MYSQLIAuth() {
 	return [
 "localhost", //mysql host
 "akshatsonar", // mysql user name
-"Play4free", // mysql user password
+"", // mysql user password
 "akshatsonarproducts" // mysql database name
 	];
 }
@@ -13,7 +13,7 @@ function PDOAuth() {
 	return [
 		"mysql:host=localhost;dbname=akshatsonarproducts", //host and database name
 		"akshatsonar", // mysql user name
-		"Play4free", // mysql user password
+		"", // mysql user password
 		[PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"]
 	];
 }
