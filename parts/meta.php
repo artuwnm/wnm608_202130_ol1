b9dcc7d9045183ba2658fbaf51c5d79689126146
vwang117 <78834354+vwang117@users.noreply.github.com>
2021-05-30 14:40:16 -0700

    Delete parts directory

--- parts/meta.php
+++
@@ -1,9 +0,0 @@
-<meta name="viewport" content="width=device-width">
-
-<base href="http://rongwdesign.com/aau/wnm608/wang.vivian/">
-
-<link rel="stylesheet" href="lib/css/styleguide.css">
-<link rel="stylesheet" href="lib/css/gridsystem.css">
-<link rel="stylesheet" href="css/storetheme.css">
-
-<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
