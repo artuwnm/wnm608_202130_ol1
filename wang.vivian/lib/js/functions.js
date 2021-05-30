a09b9272ab6640a670d1c225da963f2713b70523
vwang117 <78834354+vwang117@users.noreply.github.com>
2021-05-30 14:48:11 -0700

    Add files via upload

---
+++ wang.vivian/lib/js/functions.js
@@ -0,0 +1,16 @@
+
+const query = (options) => {
+	return fetch('data/api.php',{
+		method:'POST',
+		body: JSON.stringify(options),
+		headers:{'Content-Type':'application/json'}
+	}).then(d=>d.json());
+}
+
+
+//currying function
+const templater = f => a => 
+	(Array.isArray(a)?a:[a])
+	.reduce((r,o,i,a)=>r+f(o,i,a),'');
+
+
