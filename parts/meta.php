a3e828ec6b4e693f7f2834fa9bee26ce58a4cd5d
Oscar-Ruiquan <79069205+Oscar-Ruiquan@users.noreply.github.com>
2021-05-30 15:20:26 -0700

    Delete parts directory

--- parts/meta.php
+++
@@ -1,9 +0,0 @@
-<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-
-<base href="http://xrquandesign.com/aau/wnm608/Xu.Ruiquan/">
-
-	<link rel="stylesheet" href="lib/css/styleguide.css">
-	<link rel="stylesheet" href="lib/css/gridsystem.css">
-	<link rel="stylesheet" href="css/storetheme.css">
-
-<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
