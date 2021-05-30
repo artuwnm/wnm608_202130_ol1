99188f64d0eff5decb7bcc198a05cc9b99bb0ee7
Oscar-Ruiquan <79069205+Oscar-Ruiquan@users.noreply.github.com>
2021-05-30 15:24:51 -0700

    Add files via upload

---
+++ Xu.Ruiquan/parts/navbar.php
@@ -0,0 +1,34 @@
+<?php 
+
+include_once "lib/php/functions.php";
+
+?>
+
+
+
+
+<input type="checkbox" id="menu" class="hidden">
+<header class="navbar">
+		<div class="container display-flex">
+			<div class="flex-none">
+				<h1>NON-STOP</h1>
+			</div>
+
+		
+			<div class="flex-stretch"></div>
+			<div class="flex-none menu-button">
+				<label for="menu">&equiv;</label>
+			</div>
+			
+			<nav class="nav nav-flex flex-none">
+				<ul class="container display-flex">
+					<li><a href="index.php">Discover</a></li>
+					<li><a href="product_list.php">All</a></li>
+					<li><a href="product_cart.php">
+						<span>Cart</span>
+						<span class="badge"><?= makeCartBadge(); ?></span>
+					</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
